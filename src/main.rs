cc03bc85aaf2cd710c4659cd19be21d15c2194d4
Julian <rayconstantyjulian@gmail.com>
2025-09-26 10:34:23 +0200

    Added first input

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,12 @@
+use std::io;
 fn main() {
-    println!("Hello, world!");
+    let mut input: String = String::new();
+
+    println!("Quelle est le texte en clair ?");
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    input = input.trim().to_string();
+
+    println!("{:?}", input);
 }
